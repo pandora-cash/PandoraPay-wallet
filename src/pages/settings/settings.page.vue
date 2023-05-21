a37f2d5fb66636bef55fc8fe36ffa140d943baa6
borislavkharlamov <borislavkharlamov@proton.me>
2023-05-21 18:20:22 -0400

    Поддержка прокси

--- src/pages/settings/settings.page.vue
+++ src/pages/settings/settings.page.vue
@@ -1,15 +1,29 @@
 <template>
   <layout>
 
-    <layout-title icon="fas fa-wrench" title="Settings">Configure your web wallet for a better experience.
+    <layout-title icon="fas fa-wrench" title="Settings">
+      Configure your web wallet for a better experience.
     </layout-title>
 
     <div class="card overflow-hidden">
       <div class="card-header card-header audience-chart-header p-0 bg-light scrollbar-overlay ">
         <ul class="nav nav-tabs border-0 chart-tab flex-nowrap">
+
+          <li class="nav-item" role="presentation" @click="tab='ui'">
+            <a :class="`nav-link mb-0 ${tab === 'ui' ? 'active' : ''}`">
+              <div class="audience-tab-item p-2 pe-4 cursor-pointer">
+                <h6 class="text-800 fs--2 text-nowrap">UI</h6>
+                <h5 class="text-800 align-middle">
+                  <i class="fas fa-sliders"></i>
+                  UI Settings
+                </h5>
+              </div>
+            </a>
+          </li>
+
           <li class="nav-item" role="presentation" @click="tab='balanceDecryptor'">
             <a :class="`nav-link mb-0 ${tab === 'balanceDecryptor' ? 'active' : ''}`">
-              <div class="audience-tab-item p-2 pe-4">
+              <div class="audience-tab-item p-2 pe-4 cursor-pointer">
                 <h6 class="text-800 fs--2 text-nowrap">Balance Decryptor</h6>
                 <h5 class="text-800 align-middle">
                   <i class="fas fa-laptop-code"></i>
@@ -18,47 +32,67 @@
               </div>
             </a>
           </li>
+
           <li class="nav-item" role="presentation" @click="tab='expert'">
             <a :class="`nav-link mb-0 ${tab === 'expert' ? 'active' : ''}`">
-              <div class="audience-tab-item p-2 pe-4">
+              <div class="audience-tab-item p-2 pe-4 cursor-pointer">
                 <h6 class="text-800 fs--2 text-nowrap">Expert</h6>
                 <h5 class="text-800 align-middle">
-                  <i class="fas fa-laptop-code"></i>
+                  <i class="fas fa-cogs"></i>
                   Expert settings
                 </h5>
               </div>
             </a>
           </li>
+
         </ul>
       </div>
       <div class="card-body">
         <div class="tab-content">
+
+          <div :class="`tab-pane ${tab === 'ui' ? 'active' : ''}`">
+            <div class="row pt-2">
+              <div class="col-12 col-sm-6">
+
+                <div class="form-check" v-if="setupEnabled">
+                  <input class="form-check-input" id="setupShow" type="checkbox" v-model="setupShow"/>
+                  <label class="form-check-label" for="setupShow">Show Setup form</label>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
           <div :class="`tab-pane ${tab === 'balanceDecryptor' ? 'active' : ''}`">
             <div class="row pt-2">
               <div class="col-12 col-sm-6">
-                <label>Precomputed Table size: {{ balanceDecryptorTableSize }} <i class="fas fa-question ms-1"
-                                                                                  v-tooltip.bottom="'Balance Decryptor Precomputed Init Table'"/>
+                <label>Precomputed Table size: {{ balanceDecryptorTableSize }}
+                  <i class="fas fa-question ms-1" v-tooltip.bottom="'Balance Decryptor Precomputed Init Table'"/>
                 </label> <br/>
                 <label>Scanner Performance: {{ balanceDecryptorPerformance[balanceDecryptorTableSize] }}/s
-                  <i class="fas fa-question ms-1" v-tooltip.bottom="'Balance Decryptor performance using this precomputed table'"/> </label>
+                  <i class="fas fa-question ms-1"
+                     v-tooltip.bottom="'Balance Decryptor performance using this precomputed table'"/> </label>
                 <input class="form-range" type="range" min="16" max="22" v-model="balanceDecryptorTableSize"/>
                 <small :class="`fw-semi-bold rounded-pill badge-soft-${balanceDecryptorTableSize >= 20 ? 'danger' : 'warning'} p-1`">
                   <i class="fas fa-exclamation-triangle"/> High will require
-                  {{ $strings.formatMilliseconds(balanceDecryptorTime[balanceDecryptorTableSize] * 1000) }} initialize (bootstrap) time.
+                  {{ $strings.formatMilliseconds(balanceDecryptorTime[balanceDecryptorTableSize] * 1000) }} initialize
+                  (bootstrap) time.
                 </small>
               </div>
             </div>
           </div>
+
           <div :class="`tab-pane ${tab === 'expert' ? 'active' : ''}`">
             <div class="row pt-2">
               <div class="col-12 col-sm-6">
                 <div class="form-check">
-                  <input class="form-check-input" id="legacyNonHardening" type="checkbox" v-model="legacyNonHardening" @change="changeLegacyNonHardening"/>
+                  <input class="form-check-input" id="legacyNonHardening" type="checkbox" v-model="legacyNonHardening"/>
                   <label class="form-check-label" for="legacyNonHardening">Legacy Non Hardening</label>
                 </div>
               </div>
             </div>
           </div>
+
         </div>
       </div>
       <div class="card-footer bg-light py-2">
@@ -69,7 +103,9 @@
         <alert-box v-if="status" class="w-100" type="success" :dismissible-timeout="10000" :dismissible-text="status" @onDismissible="status=''">
           {{ status }}
         </alert-box>
-        <alert-box v-if="info" class="w-100" type="info" :dismissible-timeout="10000" :dismissible-text="info" @onDismissible="status=''"></alert-box>
+        <alert-box v-if="info" class="w-100" type="info" :dismissible-timeout="10000" :dismissible-text="info" @onDismissible="status=''">
+          {{info}}
+        </alert-box>
 
         <div class="float-end">
           <loading-button text="Save settings" :submit="handleSave" icon="fas fa-save"/>
@@ -99,6 +135,8 @@ export default {
       balanceDecryptorTableSize: 18,
       tab: "balanceDecryptor",
       legacyNonHardening: false,
+      setupEnabled: PandoraPayWalletOptions.setup.enabled,
+      setupShow: localStorage.getItem('setupSkipSetupNextTime') !== 'true',
     }
   },
 
@@ -130,14 +168,29 @@ export default {
   methods: {
 
     handleSave() {
-        this.status = ""
+      this.status = ""
+
+      if (this.tab === 'ui'){
+
+        if (this.setupEnabled){
+          localStorage.setItem('setupSkipSetupNextTime', this.setupShow === false )
+          this.status = "Setup changed table stored"
+        }
+
+      }else if (this.tab === 'balanceDecryptor'){
+
         this.$store.commit('setBalanceDecryptorTableSize', this.balanceDecryptorTableSize)
         this.status = "Balance Decryptor table stored"
-        this.info = "Restart is required to have an effect"
-    },
 
-    changeLegacyNonHardening() {
-      PandoraPay.wallet.manager.setWalletNonHardening(this.legacyNonHardening)
+      }else if (this.tab === 'expert'){
+
+        PandoraPay.wallet.manager.setWalletNonHardening(this.legacyNonHardening)
+        this.status = "Wallet non hardening stored"
+
+      }
+
+      this.info = "Restart is required to have an effect"
+
     },
 
   },
